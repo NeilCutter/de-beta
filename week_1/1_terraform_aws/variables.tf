429582d4397600d1aa7de7eccc1700e92df9b763
NeilCutter <neilcanas08@gmail.com>
2024-02-07 22:50:12 +0800

    added variables.tf file

---
+++ week_1/1_terraform_aws/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_bucket"{
+    description = "AWS Bucket"
+    default = "terraform-demo-1123-terra-bucket"
+}
+
+variable "aws_bucker_name"{
+    description = "Aws Bucket Name"
+    default = "My bucket"
+}
