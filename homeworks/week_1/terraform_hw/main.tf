71edc04077e9513bf10d996506f5ae38e9306fa3
NeilCutter <neilcanas08@gmail.com>
2024-02-18 11:10:19 +0800

    updated main.tf added variables

--- homeworks/week_1/terraform_hw/main.tf
+++ homeworks/week_1/terraform_hw/main.tf
@@ -10,8 +10,19 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+#Creating AWS s3 Bucket
 resource "aws_s3_bucket" "data-lake" {
-  bucket        = "de-zoomcamp-bucket"
-  force_destroy = True
+  bucket        = var.bucket_name
+  force_destroy = "true"
+}
+#Creating AWS redshift cluster
+resource "aws_redshift_cluster" "data-warehouse" {
+  cluster_identifier  = var.cluster_identifier
+  database_name       = var.dbname
+  master_username     = var.dbuser
+  master_password     = var.dbpassword
+  node_type           = "dc2.large"
+  cluster_type        = "single-node"
+  skip_final_snapshot = "true" #necessary if you want to destroy
 }
 
